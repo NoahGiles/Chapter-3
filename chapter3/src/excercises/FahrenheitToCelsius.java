5d9fe8c377d048658169e6da3303e413486265e3
NoahGiles <billywerber@gmail.com>
2018-09-12 09:20:14 -0500

    Updated Text

--- chapter3/src/excercises/FahrenheitToCelsius.java
+++ chapter3/src/excercises/FahrenheitToCelsius.java
@@ -9,15 +9,19 @@ public class FahrenheitToCelsius {
 		
 		double temp;
 		double temp2;
+		double temp3;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please insert a number of degrees in Fahrenheit at 8:00 >> ");
 		temp = input.nextDouble();
+		displayConversion(temp);
 		System.out.println("Please insert a number of degrees in Fahrenheit at 12:00 >> ");
 		temp2 = input.nextDouble();
-		FahrenheitToCelsius(temp);
-		
+		displayConversion(temp2);
+		System.out.println("Please insert a number of degrees in Fahrenheit at 5:00 >> ");
+		temp3 = input.nextDouble();
+		displayConversion(temp3);
 	}
-	public static void FahrenheitToCelsius(double temp) 
+	public static void displayConversion(double temp) 
 	{
 	double celsius;
 	celsius = (temp - 32) * 5/9;
